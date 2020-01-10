2198523073e3422ebc5f87fca179d823d8632971
Oreooo <yuan80056@gmail.com>
2020-01-10 17:30:16 +0800

    【C】 LoginAct：能输入东西，login按钮也能根据用户名和密码，显示透明度和能否按了

--- main/src/main/java/com/oreooo/main/LoginModel.kt
+++ main/src/main/java/com/oreooo/main/LoginModel.kt
@@ -1,23 +1,39 @@
 package com.oreooo.main
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
-import android.content.Context
+import android.databinding.Observable
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
-import android.util.Log
-import android.view.View
-import android.widget.Toast
+import android.databinding.ObservableFloat
 
 class LoginModel : ViewModel() {
 
-    private val _name = MutableLiveData<String>()
-    private val _lastName = MutableLiveData<String>()
+    //手机号码
+    val name = ObservableField<String>()
+    //验证码
+    val pwd = ObservableField<String>()
 
-     var name: LiveData<String> = _name
-     var pwd: LiveData<String> = _lastName
-
-    fun login() {
-        Log.d("12345","66666")
+    //登录按钮可点击状态
+    val loginEnable = object : ObservableBoolean(name,pwd) {
+        override fun get(): Boolean {
+            val nameStr = name.get()
+            val pwdStr = pwd.get()
+            //手机框和密码框都存在输入值时，才允许点击登录按钮
+            return if (nameStr.isNullOrEmpty() || pwdStr.isNullOrEmpty())
+                false
+            else
+                true
+        }
+    }
+    //登录按钮的透明度
+    val loginAlpha = object: ObservableFloat(loginEnable){
+        override fun get(): Float {
+            //获取按钮是否可点击的布尔值
+            val enable = loginEnable.get()
+            return if (enable)
+                1.0f
+            else
+                0.5f
+        }
     }
 }
